6946803c64d72b7388dfbfd61cb8c65d659ae57b
Benjamin Westphal <benjamin.w@tutanota.de>
2019-08-23 18:56:06 +0200

    Created data structures
    Created essential data structures Game, Board, Piece.

---
+++ entities/game.go
@@ -0,0 +1,18 @@
+package entities
+
+const (
+	MaxBoardSize    = 10
+	MinBoardSize    = 3
+	RequiredPlayers = 3
+)
+
+type Game struct {
+	Pieces
+	Board
+}
+
+type Board [][]rune
+
+type Pieces []Piece
+
+type Piece rune
