c02484b6d7608e451c7da1bfbd6b56ae2401f272
Benjamin Westphal <benjamin.w@tutanota.de>
2019-09-01 18:51:11 +0200

    Renaming and typo fix

--- game/mutating_methods.go
+++ game/mutating_methods.go
@@ -29,27 +29,6 @@ func (g *Game) PushPlayer(p Player) error {
 	return nil
 }
 
-// PushPlayer adds a Player to the list of Players. It returns an error if the
-// list is already complete, the Player already exists in the list or if the
-// Player is illegal.
-func (ps Players) PushPlayer(p Player) (Players, error) {
-	if len(ps) >= RequiredNumberOfPlayers {
-		return ps, ErrGameStarted
-	}
-
-	for i, _ := range ps {
-		if ps[i] == p {
-			return ps, ErrPlayerExists
-		}
-	}
-
-	if p == NoPlayer {
-		return ps, ErrPlayerIllegal
-	}
-
-	return append(ps, p), nil
-}
-
 // PushMove adds a Move to the History. It returns an error if it is illegal or
 // if we are still waiting for other Players to join.
 //
@@ -71,10 +50,31 @@ func (g *Game) PushMove(m Move) error {
 
 	ok = g.size.doesSquareExist(m.X, m.Y)
 	if !ok {
-		return ErrMoveSquareNotExist
+		return ErrMoveSquareDoesNotExist
 	}
 
 	g.history = append(g.history, m)
 
 	return nil
+}
+
+// PushPlayer adds a Player to the list of Players. It returns an error if the
+// list is already complete, the Player already exists in the list or if the
+// Player is illegal.
+func (ps Players) PushPlayer(p Player) (Players, error) {
+	if len(ps) >= RequiredNumberOfPlayers {
+		return ps, ErrGameStarted
+	}
+
+	for i, _ := range ps {
+		if ps[i] == p {
+			return ps, ErrPlayerExists
+		}
+	}
+
+	if p == NoPlayer {
+		return ps, ErrPlayerIllegal
+	}
+
+	return append(ps, p), nil
 }
